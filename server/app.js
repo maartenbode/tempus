83240a19460039f58d3202f07b9f13d898e7df2a
Maarten Bode <maartenbodepc@gmail.com>
2017-01-29 15:25:55 +0100

    Add API route for fetching all time entries
    These time entries are grouped by their start date.

--- server/app.js
+++ server/app.js
@@ -1,22 +1,42 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const lodash = require('lodash');
+const moment = require('moment');
 const TimeEntry = require('./models/TimeEntry');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+// Use body parsing
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Display a listing of all time entries
+app.get('/api/time-entry', (req, res) => {
+  TimeEntry
+  .find()
+  .sort({ started_at: -1 })
+  .exec((err, entries) => {
+    res.json(lodash.groupBy(entries, (entry) => {
+      return moment(entry.started_at).format('YYYY-MM-DD');
+    }));
+  });
 });
 
+// Create a new time entry
 app.post('/api/time-entry', (req, res) => {
-  const timeEntry = new TimeEntry({
-    description: 'Marketing plan schrijven',
-    duration: 60
-  });
-
+  const timeEntry = new TimeEntry(req.body);
   timeEntry.save();
 
   res.json(timeEntry);
 });
 
+// Delete the given time entry
+app.delete('/api/time-entry/:id', (req, res) => {
+  TimeEntry.remove({ _id: req.params.id }).exec();
+
+  res.sendStatus(200);
+});
+
+// Boot server
 app.listen(4000, () => {
   console.log('Example app listening on port 4000!');
 });
--- server/models/TimeEntry.js
+++ server/models/TimeEntry.js
@@ -1,9 +1,9 @@
 const mongoose = require('../connection');
 
 const timeEntrySchema = new mongoose.Schema({
-  description:  String,
+  description: { type: String, default: '-' },
   started_at: { type: Date, default: Date.now },
-  duration: Number
+  duration: { type: Number, default: 0 },
 });
 
 const TimeEntry = mongoose.model('time_entries', timeEntrySchema);
